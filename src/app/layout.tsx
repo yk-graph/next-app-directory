41fc46d7790b8e6f1ea862a89081653095bcdcea
yk-graph <yokokura.tatsuya@gmail.com>
2023-07-09 12:23:25 +0900

    base setting

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
 const inter = Inter({ subsets: ['latin'] })
+
+import NavBar from '@/components/NavBar'
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
     </html>
   )
 }
